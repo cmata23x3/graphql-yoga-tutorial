d3233bb1737e4431b477fca29589ac8e8f64e5cf
Christian Mata <christian.m.mata@gmail.com>
2023-07-03 17:55:40 -0500

    Add a simple query with in-memory data

--- src/schema.ts
+++ src/schema.ts
@@ -5,16 +5,43 @@ import { makeExecutableSchema } from "@graphql-tools/schema"
 */
 const typeDefinitions = `
     type Query {
-        hello: String!
+        info: String!
+        feed: [Link!]!
+    }
+
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
     }
 `
 
 /**
  * Resolvers are used for resolving the data. 
  */
+type Link = {
+    id: string
+    url: string
+    description: string
+}
+
+const links: Link[] = [
+    {
+        id: 'link-0',
+        url: 'https://graphql-yoga.com',
+        description: 'The easiest way of setting up a GraphQL server',
+    }
+]
+
 const resolvers = {
     Query: {
-        hello: () => 'Hello World!'
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links
+    },
+    Link: {
+        id: (parent: Link) => parent.id,
+        description: (parent: Link) => parent.description,
+        url: (parent: Link) => parent.url,
     }
 }
 
