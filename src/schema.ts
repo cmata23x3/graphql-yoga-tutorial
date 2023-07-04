065bbdfeab003bdd2a9a11c16fdcc4bd1a7cee9a
Christian Mata <christian.m.mata@gmail.com>
2023-07-04 01:46:04 -0500

    Extend the graph to have comments manipulations and queries

--- src/schema.ts
+++ src/schema.ts
@@ -9,16 +9,24 @@ const typeDefinitions = `
     type Query {
         info: String!
         feed: [Link!]!
+        comment(id: ID!): Comment
     }
 
     type Mutation {
         postLink(url: String!, description: String!): Link!
+        postCommentOnLink(linkId: ID!, body: String!): Comment!
     }
 
     type Link {
         id: ID!
         description: String!
         url: String!
+        comments: [Comment!]!
+    }
+
+    type Comment {
+        id: ID!
+        body: String!
     }
 `
 
@@ -30,7 +38,27 @@ const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: (parent: unknown, args: {}, context: GraphQLContext) =>
-            context.prisma.link.findMany()
+            context.prisma.link.findMany(),
+        async comment(
+            parent: unknown,
+            args: { id: string },
+            context: GraphQLContext
+        ) {
+            return context.prisma.comment.findUnique({
+                where: {
+                    id: parseInt(args.id)
+                }
+            })
+        },
+    },
+    Link: {
+        comments(parent: Link, args: {}, context: GraphQLContext) {
+            return context.prisma.comment.findMany({
+                where: {
+                    linkId: parent.id,
+                }
+            })
+        }
     },
     Mutation: {
         async postLink(
@@ -43,9 +71,22 @@ const resolvers = {
                     url: args.url,
                     description: args.description,
                 }
-        })
+            })
             return newLink  
         },
+        async postCommentOnLink(
+            parent: unknown,
+            args: { linkId: string; body: string },
+            context: GraphQLContext,
+        ) {
+            const newComment = await context.prisma.comment.create({
+                data: {
+                    linkId: parseInt(args.linkId),
+                    body: args.body,
+                }
+            })
+            return newComment
+        }
     },
 }
 
